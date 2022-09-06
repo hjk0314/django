220a7630264579a9832f271d82f292f35855efa3
hjk0314 <hjk0314@gmail.com>
2022-09-06 19:10:10 +0900

    django tutorial is end

--- myApp/urls.py
+++ myApp/urls.py
@@ -5,5 +5,7 @@ from myApp import views
 urlpatterns = [
     path('', views.index),
     path('create/', views.create),
-    path('read/<id>/', views.read)
+    path('read/<id>/', views.read), 
+    path('update/<id>/', views.update), 
+    path('delete/', views.delete)
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.views.decorators.csrf import csrf_exempt
 
 
+nextId = 4
 topics = [
     {'id': 1, 'title': 'Routing', 'body': 'Routing is ...'}, 
     {'id': 2, 'title': 'View', 'body': 'View is ...'}, 
@@ -8,8 +10,21 @@ topics = [
 ]
 
 
-def htmlTemplate(article):
+def htmlTemplate(article, id=None):
     global topics
+    contextUI = ''
+    if id != None:
+        contextUI = f'''
+            <li>
+                <form action="/delete/" method="POST">
+                    <input type="hidden" name="id" value={id}>
+                    <input type="submit" value="delete">
+                </form>
+            </li>
+            <li>
+                <a href="/update/{id}">update</a>
+            </li>
+        '''
     ol = ''
     for i in topics:
         ol += f'<li><a href="/read/{i["id"]}">{i["title"]}</a></li>'
@@ -21,6 +36,10 @@ def htmlTemplate(article):
                 {ol}
             </ul>
             {article}
+            <ul>
+                <li><a href='/create/'>create</a></li>
+                {contextUI}
+            </ul>
         </body>
         </html>
     '''
@@ -40,8 +59,71 @@ def read(request, id):
     global topics
     article = [f'<h2>{i["title"]}</h2>{i["body"]}' for i in topics if i["id"] == int(id)]
     result = ''.join(article)
-    return HttpResponse(htmlTemplate(result))
+    return HttpResponse(htmlTemplate(result, id))
 
 
+@csrf_exempt
 def create(request):
-    return HttpResponse('Create')
+    global nextId
+    if request.method == 'GET':
+        article = '''
+            <form action="/create/" method="post">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(htmlTemplate(article))
+    elif request.method == 'POST':
+        title = request.POST['title']
+        print(title)
+        body = request.POST['body']
+        print(body)
+        newTopic = {"id": nextId, "title": title, "body": body}
+        topics.append(newTopic)
+        url = '/read/' + str(nextId)
+        nextId += 1
+        return redirect(url)
+    else:
+        pass
+
+
+@csrf_exempt
+def update(request, id):
+    global topics
+    if request.method == "GET":
+        for i in topics:
+            if i['id'] == int(id):
+                selectedTopic = {
+                    "title": i["title"], 
+                    "body": i["body"]
+                }
+        article = f'''
+            <form action="/update/{id}/" method="post">
+                <p><input type="text" name="title" placeholder="title" value={selectedTopic["title"]}></p>
+                <p><textarea name="body" placeholder="body">{selectedTopic["body"]}</textarea></p>
+                <p><input type="submit"></p>
+            </form>
+        '''
+        return HttpResponse(htmlTemplate(article, id))
+    elif request.method == "POST":
+        title = request.POST['title']
+        body = request.POST['body']
+        for i in topics:
+            if i["id"] == int(id):
+                i["title"] = title
+                i["body"] = body
+        return redirect(f'/read/{id}')
+
+
+@csrf_exempt
+def delete(request):
+    global topics
+    if request.method == "POST":
+        id = request.POST['id']
+        newTopics = []
+        for i in topics:
+            if i["id"] != int(id):
+                newTopics.append(i)
+        topics = newTopics
+        return redirect("/")
